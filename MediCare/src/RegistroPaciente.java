fb48173275ce69ebfca2cf7ca2c4a0466d784981
WALTER\Soledad Cobacango <john.cobacango@epn.edu.ec>
2024-07-27 22:48:53 -0500

    Creaciion de interfaces

---
+++ MediCare/src/RegistroPaciente.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class RegistroPaciente {
+    private JPanel panelRegistroPac;
+    private JLabel titRegistro;
+    private JButton IngresoMayorEdad;
+    private JButton IngresoMenorEdad;
+    private JButton volver;
+}
